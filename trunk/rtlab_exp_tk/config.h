7fcb0e8663b2e2fc24e1badd221cb0eb3cbbbfd0
calin <calin@3478f4b9-7146-0410-9890-c50be665d8da>
2002-09-18 23:10:00 +0000

    Various tweaks

--- trunk/rtlab_exp_tk/config.h
+++ trunk/rtlab_exp_tk/config.h
@@ -7,24 +7,36 @@
 #define DAQ_DIRNAME "daq_system"
 #endif
 
-#ifndef DAQ_PREFIX
-#define DAQ_PREFIX "/usr/local/"
+#ifndef RTLAB_PREFIX
+#define RTLAB_PREFIX "/opt/rtlab/"
+#endif
+
+#ifndef RTLAB_DATA_PREFIX
+#define RTLAB_DATA_PREFIX "/tmp/"
 #endif
 
 #ifndef DAQ_DATA_PREFIX
-#define DAQ_DATA_PREFIX "/tmp/"
+#define DAQ_DATA_PREFIX RTLAB_DATA_PREFIX
 #endif
 
-#ifndef DAQ_ETC_PREFIX
-#define DAQ_ETC_PREFIX DAQ_PREFIX "/etc/" DAQ_DIRNAME
+#ifndef RTLAB_ETC_PREFIX
+#define RTLAB_ETC_PREFIX RTLAB_PREFIX "/etc/" 
 #endif
 
-#ifndef DAQ_RESOURCES_PREFIX
-#define DAQ_RESOURCES_PREFIX DAQ_PREFIX "/lib/" DAQ_DIRNAME
+#ifndef RTLAB_RESOURCES_PREFIX
+#define RTLAB_RESOURCES_PREFIX RTLAB_PREFIX 
 #endif
 
-#ifndef DAQ_PLUGINS_PREFIX
-#define DAQ_PLUGINS_PREFIX DAQ_RESOURCES_PREFIX "/plugins/"
+#ifndef DAQ_LOG_TEMPLATES_PREFIX
+#define DAQ_LOG_TEMPLATES_PREFIX RTLAB_RESOURCES_PREFIX
+#endif
+
+#ifndef RTLAB_PLUGINS_PREFIX
+#define RTLAB_PLUGINS_PREFIX RTLAB_RESOURCES_PREFIX "/plugins/"
+#endif
+
+#ifndef RTLAB_MODULES_PREFIX
+#define RTLAB_MODULES_PREFIX RTLAB_RESOURCES_PREFIX "/modules/"
 #endif
 
 #endif
