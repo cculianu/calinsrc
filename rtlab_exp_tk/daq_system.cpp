320fc5c7e98b514097471780bbbe5a34a0d743aa
calin <calin@3478f4b9-7146-0410-9890-c50be665d8da>
2003-01-29 16:33:30 +0000

    GCC3.2 fixups

--- rtlab_exp_tk/daq_system.cpp
+++ rtlab_exp_tk/daq_system.cpp
@@ -1711,7 +1711,7 @@ Plugin * PluginMenu::pluginFindByName(QString name)
 
 
 WindowTemplateDialog::WindowTemplateDialog(DAQSystem *parent, 
-                                           const char *name = 0, WFlags f = 0)
+                                           const char *name, WFlags f)
   : QWidget (parent, name, f)
 {
   setIcon(QPixmap(DAQImages::wintemplates_img));
